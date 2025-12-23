78297da469e61e5fe4db8450fc9a3ef9cd51cf5e
sujin-1111 <sujin3421192@gmail.com>
2025-12-23 21:44:38 +0900

    (2025.12.23) 범용 포인터

--- programe/source.c
+++ programe/source.c
@@ -35,26 +35,78 @@ void main()
 #pragma region 포인터
 	// 메모리의 주소 값을 저장할 수 있는 변수입니다.
 	
-	int age = 8;
-	int * pointer = &age;
-	
-	// 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로
-	// 포인터가 가리키는 메모리의 자료형을 선언해주어야함.
-	printf("age = %p\n", &age);
-	printf("pointer의 값 = %p\n", pointer);
-	printf("pointer의 주소 = %p\n", &pointer);
-	
-	*pointer = 99;
-	printf("pointer가 가리키는 값 = %d\n", *pointer);
-
-	int room;
-	*pointer = room;
-
-	printf("room의 주소 = %p\n", &room);
-
+	// int age = 8;
+	// int room = 20;
+	// int * pointer = &age;
+	// 
+	// // 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로
+	// // 포인터가 가리키는 메모리의 자료형을 선언해주어야함.
+	// printf("age = %p\n", &age);
+	// printf("pointer의 값 = %p\n", pointer);
+	// printf("pointer의 주소 = %p\n\n", &pointer);
+	// 
+	// *pointer = 99;
+	// printf("pointer가 가리키는 값 = %d\n\n", *pointer);
+	// 
+	// 
+	// pointer = &room;
+	// 
+	// printf("pointer의 값 = %p\n", pointer);
+	// printf("room의 주소 = %d\n\n", *pointer);
+	// 
+	// // 포인터 변수도 자신의 메모리 공간을 가지고 있으며, 포인터 변수에
+	// // 변수의 주소를 저장되면 해당 변수의 시작 주소를 가리킵니다.
+	// 
+	// printf("int의 크기 = %u\n\n", sizeof(int));
+	// printf("pointer 변수의 크기 = %p\n", sizeof(pointer));
+	// 
+	// // 포인터 변수의 크기는 중앙 처리 장치가 한 번에 처리할 수 이쓴ㄴ 
+	// // 크기로 정해지며, 한번에 처리할 수 있는 크기는 운영체제에 따라 크기가 변경됨
 #pragma endregion
 
+#pragma region 범용 포인터
+	// 자료형이 정해지지 않은 상태로 모든 자료형을 
+	// 저장할 수 있는 포인터.
 
 
+
+	// char rank = 'A';
+	// int age = 10;
+	// float weight = 35.75f;
+	// 
+	// void * reference = NULL;
+	// 
+	// reference = &rank;
+	// *(char *)reference = 'B';
+	// 
+	// reference = &age;
+	// *(int*)reference = 20;
+	// 
+	// reference = &weight;
+	// *(float*)reference = 65.6;
+	// 
+	// printf("rank = %c\n", rank);
+	// printf("age = %d\n", age);
+	// printf("weight = %.1f\n", weight);
+	
+	// 범용 포인터는 메모리 주소에 접근해서 값을 변경할 수 없음.
+	
+	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가
+	// 가리키는 변수의 자료형으로 변환해주어야함.
+#pragma endregion
+
+#pragma region 상수 지시 포인터
+	// 포인터가 가리키는 주소에 지정되어 있는
+
+	int z = 20;
+	int y = 10;
+
+	const int* fixedPointer = &y;
+
+
+
+
+
+#pragma endregion
 
 }
