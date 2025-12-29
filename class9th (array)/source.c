bab6b886c8487a7bc4b2f86c1e39be03d8aea216
sujin-1111 <sujin3421192@gmail.com>
2025-12-29 21:44:21 +0900

    (2025.12.29) 함수

---
+++ class9th (array)/source.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region 배열
+	// // 같은 자료형의 변수들로 이루어진 유한 집합
+	// 
+	// int list[5];
+	// 
+	// // 배열의 경우 쳣 번째 원소는 0부터 시작
+	// 
+	// for (int i = 0; i < 5; i++)
+	// {
+	// 		list[i] = (i + 1) * 10;
+	// 	printf("list[%d] : %d\n", i, list[i]);
+	// }
+	// 
+	// // 배열의 메모리 공간을 프로그램이 
+	// // 실행되는 동안 변경할수없음
+
+	// int array[] = { 1,2,3,4,5 };
+	// 
+	// int size = (int)sizeof(array) / sizeof(array[0]);
+	// 
+	// for (int i = 0; i < size; i++)
+	// {
+	// 	printf("array[%d]의 값 : %d\n", i, (array[i]));
+	// }
+	// 
+	// // 배열의 크기는 생략할 수 있으며, 초기화 목록에서
+	// // 설정한 요소에 따라 배열의 크기가 결정됨
+	// 
+	// int* pointer = &array;
+
+#pragma endregion
+
+#pragma region 아스키코드
+
+#pragma endregion
+
+
+
+}
--- programe/source.c
+++ programe/source.c
@@ -1,43 +1,76 @@
 #include <stdio.h>
 
+void initialize()
+{
+	printf("초기화");
+}
+
+void coordinate(int x, int y)
+{
+	printf("좌표 = x : %d, y : %d \n", x, y);
+}
+
+void reset(char grade, int level, float experience)
+{
+	printf("grade : %c level %d: experience : %f", grade, level, experience);
+}
+
+void swap(int argumentX, int argumentY)
+{
+	int temporary = argumentY;
+	argumentY = argumentX;
+	argumentX = argumentY;
+	
+}
+
+
+
 void main()
 {
-#pragma region 배열
-	// // 같은 자료형의 변수들로 이루어진 유한 집합
-	// 
-	// int list[5];
-	// 
-	// // 배열의 경우 쳣 번째 원소는 0부터 시작
-	// 
-	// for (int i = 0; i < 5; i++)
-	// {
-	// 		list[i] = (i + 1) * 10;
-	// 	printf("list[%d] : %d\n", i, list[i]);
-	// }
-	// 
-	// // 배열의 메모리 공간을 프로그램이 
-	// // 실행되는 동안 변경할수없음
+#pragma region 함수
+	// 하나의 특별한 목적의 작업을 수행하기위해
+	// 독립적으로 설계된 코드의 집합.
 
-	int array[] = { 1,2,3,4,5 };
-
-	int size = (int)sizeof(array) / sizeof(array[0]);
-
-	for (int i = 0; i < size; i++)
-	{
-		printf("array[%d]의 값 : %d\n", i, (array[i]));
-	}
-	
-	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서
-	// 설정한 요소에 따라 배열의 크기가 결정됨
-
-	int* pointer = &array;
-	
-
-
-
+	// initialize();
 
 
 #pragma endregion
+
+#pragma region 매개 변수
+	// 함수의 정의에서 전달받은 함수를 내부로
+	// 전달하기위해 사용하는 변수
+
+	// coordinate(10, 20);
+
+	// 매개변수는 함수 내부에서만 연산이 이루어지며
+	// 함수가 종료되면 메모리에서 해제됨
+	
+	// reset('f', 0, 0.0);
+
+	// 하나의 함수에 서로 다른 자료형의 매개면수를 함께
+	// 정의할 수 잇으며, 여러개의 매개변수를 생성할수맀음
+#pragma endregion
+
+#pragma region 인수
+	// 함수가 호출될 때 매개 변수에 실제로 전달되는 값
+	
+	int x = 10;
+	int y = 20;
+	swap(x, y);
+	
+
+	printf("x : %d\n", x);
+    printf("y : %d\n", y);
+
+	// 인수의 셩우 함수에 있는 매개변수에따라 전달할 수 있는
+	// 인수의 수가 결정되며 값을 일정하게 
+	
+	// [ call by reference ]
+	
+
+
+#pragma endregion
+
 
 
 }
